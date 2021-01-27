631e888437330db260dfcbbc6f449f2703f47684
anderbecks <ludviganderbeck@gmail.com>
2021-01-27 14:38:41 +0100

    added remove book for librarian

--- LibraryAppProject/src/com/company/Main.java
+++ LibraryAppProject/src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.Entities.Librarian;
 import com.company.Helpers.MenuHelper;
 import com.company.Menus.AdminMenu;
 import com.company.Menus.MainMenu;
@@ -8,9 +9,15 @@ import com.company.Menus.UserMenu;
 public class Main {
 
     public static void main(String[] args) {
+        Librarian.librarianAddBook();
+        Librarian.librarianAddBook();
+        System.out.println(Library.bookList);
+        Librarian.librarianRemoveBook();
+        System.out.println(Library.bookList);
 
         MenuHelper.initMenu(MainMenu.values());
         MenuHelper.initMenu(AdminMenu.values());
         MenuHelper.initMenu(UserMenu.values());
     }
+
 }
