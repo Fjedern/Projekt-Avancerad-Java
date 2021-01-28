5aa7862a90162e029906efcf1c3a1ecfe9a35223
David N <david@nlsn.nu>
2021-01-28 09:36:11 +0100

    cleaning

--- LibraryAppProject/src/com/company/Library.java
+++ LibraryAppProject/src/com/company/Library.java
@@ -7,18 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    public Library() { }
+    public Library() {
+    }
 
     public static List<Book> bookList = new ArrayList<>();
-    public static List <User> userlist = new ArrayList<>();
+    public static List<User> userlist = new ArrayList<>();
 
-    public static void addBook(Book book){
+    public static void addBook(Book book) {
         bookList.add(book);
     }
-    public static void removeBook(String bookTitleToRemove){
-        bookList.removeIf(book -> book.getTitle().equalsIgnoreCase(bookTitleToRemove));
-            }
-        }
-    }
 
+    public static void removeBook(String bookTitleToRemove) {
+        bookList.removeIf(book -> book.getTitle().equalsIgnoreCase(bookTitleToRemove));
+    }
 }
