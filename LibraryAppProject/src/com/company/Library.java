f52777a9c151bcc3b57c61c3d8f128170084d9f4
Oskar Andersson <oskand01@gmail.com>
2021-01-28 12:51:12 +0100

    searchFunctions added

--- LibraryAppProject/src/com/company/Library.java
+++ LibraryAppProject/src/com/company/Library.java
@@ -2,15 +2,108 @@ package com.company;
 
 import com.company.Entities.Book;
 import com.company.Entities.User;
+import com.company.Helpers.MenuHelper;
+
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
+
+    public static List<Book> bookList = new ArrayList<>();
+    public static List<User> userList = new ArrayList<>();
+    MenuHelper menuHelper;
+
+
+
     public Library() {
     }
 
-    public static List<Book> bookList = new ArrayList<>();
-    public static List<User> userlist = new ArrayList<>();
 
+
+
+    //Funktionen som kör igång programmet
+    public void openLibrary() {
+
+    }
+
+    public void showAllBooks() {
+        bookList.sort(Comparator.comparing(Book::getTitle));
+        for (Book book : bookList) {
+            System.out.println("* " + book.getTitle() + " by " + book.getAuthor());
+        }
+    }
+
+    public void searchBookByTitle() {
+        Scanner scan = new Scanner(System.in);
+        int matches = 0;
+        System.out.print("Search books by title: ");
+
+        try {
+            String regex = scan.nextLine();
+            Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+            bookList.sort(Comparator.comparing(Book::getTitle));
+
+            System.out.println("\nBooks matching: '" + regex + "'");
+            for (Book book : bookList) {
+                Matcher matcher = pattern.matcher(book.getTitle());
+                boolean matchFound = matcher.find();
+
+                if(matchFound) {
+                    matches++;
+                    System.out.println("* " + book.getTitle().toUpperCase() + " by " + book.getAuthor());
+                }
+            }
+
+            if (matches == 0) {
+                System.out.println("No books matches your search");
+            }
+
+        } catch (Exception e) {
+            searchBookByTitle();
+        }
+    }
+
+    public void searchBookByAuthor() {
+        Scanner scan = new Scanner(System.in);
+        int matches = 0;
+        System.out.print("Search books by author: ");
+
+        try {
+            String regex = scan.nextLine();
+            Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+            bookList.sort(Comparator.comparing(Book::getTitle));
+
+            System.out.println("\nAuthors matching: '" + regex + "'");
+            for (Book book : bookList) {
+                Matcher matcher = pattern.matcher(book.getAuthor());
+                boolean matchFound = matcher.find();
+
+                if(matchFound) {
+                    matches++;
+                    System.out.println("* " + book.getTitle() + " by " + book.getAuthor().toUpperCase());
+                }
+            }
+
+            if (matches == 0) {
+                System.out.println("No authors matches your search");
+            }
+
+        } catch (Exception e) {
+            searchBookByTitle();
+        }
+    }
+
+
+    public void addBookToList(Book book) {
+        bookList.add(book);
+    }
+
+    public void addUserToList(User user) {
+        userList.add(user);
+    }
 }
--- LibraryAppProject/src/com/company/Main.java
+++ LibraryAppProject/src/com/company/Main.java
@@ -17,17 +17,46 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        Librarian.librarianAddBook();
+        /*Librarian.librarianAddBook();
         Librarian.librarianAddBook();
         System.out.println(Library.bookList);
         Librarian.librarianRemoveBook();
         System.out.println(Library.bookList);
 
+
+
+
+        menuHelper.initMenu(AdminMenu.values());
+        menuHelper.initMenu(UserMenu.values());*/
+        Library library = new Library();
         MenuHelper menuHelper = new MenuHelper();
 
+        Book b1 = new Book("Sagan om ringen", "Frodo walking and having fun with his friends","J.R.R. Tolkien");
+        Book b2 = new Book("Sagan om de två tornen", "Frodo having some more fun with his friends","J.R.R. Tolkien");
+        Book b3 = new Book("Sagan om de konungens återkomst", "Frodo having even more fun with his friends","J.R.R. Tolkien");
+        Book b4 = new Book("Papillion", "Papillion having fun on his own","Henri Charriere");
+        Book b5 = new Book("Fakta om Finland", "Interesting fact about Finland","Erland Loe");
+        Book b6 = new Book("Oryx and Crake", "A story about life in the future","Margaret Atwood");
+
+        library.addBookToList(b1);
+        library.addBookToList(b2);
+        library.addBookToList(b3);
+        library.addBookToList(b4);
+        library.addBookToList(b5);
+        library.addBookToList(b6);
+
+        /*library.showAllBooks();
+        library.searchBookByTitle();
+        library.searchBookByAuthor();
+*/
+
         menuHelper.initMenu(MainMenu.values());
-        menuHelper.initMenu(AdminMenu.values());
-        menuHelper.initMenu(UserMenu.values());
+
+
+
+
+
+
 
 
 
