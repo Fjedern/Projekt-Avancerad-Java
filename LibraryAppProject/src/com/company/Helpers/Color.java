a54646ab593b5f3b19cb1d9f46f1e3df1350d7ec
David N <david@nlsn.nu>
2021-02-03 09:24:44 +0100

    Small fixes

--- LibraryAppProject/src/com/company/Helpers/Color.java
+++ LibraryAppProject/src/com/company/Helpers/Color.java
@@ -8,5 +8,6 @@ public class Color {
     public static final String BLUE = "\u001B[34m";
     public static final String PURPLE = "\u001B[35m";
     public static final String CYAN = "\u001B[36m";
+    public static final String YELLOW = "\u001B[33m";
 
 }
--- LibraryAppProject/src/com/company/Library.java
+++ LibraryAppProject/src/com/company/Library.java
@@ -8,10 +8,7 @@ import com.company.Menus.AdminMenu;
 import com.company.Menus.MainMenu;
 import com.company.Menus.UserMenu;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
